6c885e1e2d82cc371c53427ba948d5376ca549c3
KovbenyaAlexander <kovbenya.alexander1@gmail.com>
2023-02-26 12:23:17 +0300

    add chakra, firebase and recoil

--- reddit-client/src/pages/index.tsx
+++ reddit-client/src/pages/index.tsx
@@ -1,11 +1,10 @@
 import Head from "next/head";
 import Image from "next/image";
-import styles from "@/styles/Home.module.css";
 
 export default function Home() {
   return (
     <div>
-      <p>Hi</p>
+      <p>Hi asdgdsth fghjdfgh fSDASDE FDSFD SFDFS DF SDFSDFCSDXC</p>
     </div>
   );
 }
