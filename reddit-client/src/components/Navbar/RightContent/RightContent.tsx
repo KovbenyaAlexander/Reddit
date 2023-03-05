6526a93d8a539d85a55ce775d0819165960a4258
KovbenyaAlexander <kovbenya.alexander1@gmail.com>
2023-03-05 17:39:42 +0300

    implement logout

--- reddit-client/src/components/Navbar/RightContent/RightContent.tsx
+++ reddit-client/src/components/Navbar/RightContent/RightContent.tsx
@@ -1,17 +1,33 @@
-import { Flex } from "@chakra-ui/react";
+import { Button, Flex } from "@chakra-ui/react";
 import React from "react";
 import AuthButtons from "./AuthButtons";
 import AuthModal from "@/components/Modal/Auth/AuthModal";
+import { auth } from "../../../firebase/clientApp";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { signOut } from "firebase/auth";
 
 type RightContentProps = {};
 
 const RightContent: React.FC<RightContentProps> = () => {
+  const [user] = useAuthState(auth);
+
   return (
     <>
       <AuthModal />
       {/* @ts-ignore*/}
       <Flex justify="center" align="center"></Flex>
-      <AuthButtons />
+
+      {user ? (
+        <Button
+          onClick={() => {
+            signOut(auth);
+          }}
+        >
+          signout
+        </Button>
+      ) : (
+        <AuthButtons />
+      )}
     </>
   );
 };
