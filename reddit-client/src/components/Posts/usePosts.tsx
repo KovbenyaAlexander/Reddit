0193a084eb4986aa447f059ac793f0bbb9f88505
KovbenyaAlexander <kovbenya.alexander1@gmail.com>
2023-04-15 13:12:32 +0300

    implement getting postVotes after reloading the page

--- reddit-client/src/components/Posts/usePosts.tsx
+++ reddit-client/src/components/Posts/usePosts.tsx
@@ -1,16 +1,26 @@
+import { CommunityState } from "@/atoms/communitiesAtom";
 import { IPost, IPostVote, postState } from "@/atoms/postsAtom";
 import { auth, firestore, storage } from "@/firebase/clientApp";
-import { collection, deleteDoc, doc, writeBatch } from "firebase/firestore";
+import {
+  collection,
+  deleteDoc,
+  doc,
+  getDocs,
+  query,
+  where,
+  writeBatch,
+} from "firebase/firestore";
 import { deleteObject, ref } from "firebase/storage";
-import React from "react";
+import React, { useEffect } from "react";
 import { useAuthState } from "react-firebase-hooks/auth";
-import { useRecoilState } from "recoil";
+import { useRecoilState, useRecoilValue } from "recoil";
 
 type usePostsProps = {};
 
 const usePosts = () => {
   const [postStateValue, setPostStateValue] = useRecoilState(postState);
   const [user] = useAuthState(auth);
+  const communityState = useRecoilValue(CommunityState);
 
   const onVote = async (post: IPost, vote: number, communityId: string) => {
     try {
@@ -116,6 +126,29 @@ const usePosts = () => {
       return false;
     }
   };
+
+  const getCommunityPostVotes = async (communityId: string) => {
+    const postVoteQuery = query(
+      collection(firestore, "users", `${user?.uid}/postVotes`),
+      where("communityId", "==", communityId)
+    );
+
+    const postVoteDocs = await getDocs(postVoteQuery);
+    const postVotes = postVoteDocs.docs.map((doc) => ({
+      id: doc.id,
+      ...doc.data(),
+    }));
+
+    setPostStateValue((prev) => {
+      return { ...prev, postVotes: postVotes as IPostVote[] };
+    });
+  };
+
+  useEffect(() => {
+    if (user && communityState?.currentCommunity?.id) {
+      getCommunityPostVotes(communityState.currentCommunity.id);
+    }
+  }, [communityState, user]);
 
   return {
     postStateValue,
