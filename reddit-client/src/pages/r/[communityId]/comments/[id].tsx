3af49886eabb698a0c6eb826cc6238cbf87b1cd6
KovbenyaAlexander <kovbenya.alexander1@gmail.com>
2023-04-16 11:18:29 +0300

    implement fetching a post after reboot the page

--- reddit-client/src/pages/r/[communityId]/comments/[id].tsx
+++ reddit-client/src/pages/r/[communityId]/comments/[id].tsx
@@ -1,14 +1,41 @@
 import PageContent from "@/components/Layout/PageContent";
 import PostItem from "@/components/Posts/PostItem";
 import usePosts from "@/components/Posts/usePosts";
-import { auth } from "@/firebase/clientApp";
-import React from "react";
+import { auth, firestore } from "@/firebase/clientApp";
+import { doc, getDoc } from "firebase/firestore";
+import { useRouter } from "next/router";
+import React, { useEffect } from "react";
 import { useAuthState } from "react-firebase-hooks/auth";
 
 const PostPage: React.FC = () => {
   const [user] = useAuthState(auth);
   const { postStateValue, onDeletePost, onVote, setPostStateValue } =
     usePosts();
+  const router = useRouter();
+
+  const fetchPost = async (postId: string) => {
+    try {
+      const postDocRef = doc(firestore, "posts", postId);
+      const postDoc = await getDoc(postDocRef);
+
+      setPostStateValue((prev) => {
+        return {
+          ...prev,
+          selectedPost: { id: postDoc.id, ...postDoc.data() } as IPost,
+        };
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    const { id } = router.query;
+
+    if (id && !postStateValue.selectedPost) {
+      fetchPost(id as string);
+    }
+  }, [router.query, postStateValue.selectedPost]);
 
   return (
     <PageContent>
